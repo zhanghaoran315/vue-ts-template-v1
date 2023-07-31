8a274009b58e15ddd9f98481a3c89b146d443e52
张浩然 <zhanghaoran@greatmap.com.cn>
2023-07-31 11:51:56 +0800

    feat：计数器补充

--- src/views/logManage/SystemLog/SystemLog.vue
+++ src/views/logManage/SystemLog/SystemLog.vue
@@ -1,13 +1,27 @@
 <script lang="ts" setup>
-onMounted(() => {
-  console.log('系统日志')
-})
+import { h, ref } from 'vue'
+import { ElButton } from 'element-plus'
+
+const counter = ref(0)
+
+const RenderCounter = () => {
+  return h('div', { class: 'counter' }, [
+    h(ElButton, { type: 'primary', onClick: () => counter.value-- }, '-1'),
+    h('span', `当前计数${counter.value}`),
+    h(ElButton, { type: 'primary', onClick: () => counter.value++ }, '+1')
+  ])
+}
 </script>
 
 <template>
   <div class="system-log">
-    <h2>system-log</h2>
+    <h2>渲染函数写的计数器</h2>
+    <RenderCounter />
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.system-log {
+  padding: 20px;
+}
+</style>
--- src/views/logManage/TaskException/TaskException.vue
+++ src/views/logManage/TaskException/TaskException.vue
@@ -1,16 +1,29 @@
-<script lang="ts" setup>
-onMounted(() => {
-  console.log('任务一场')
-})
-  
+<script lang="tsx" setup>
+import { h, ref } from 'vue'
+import { ElButton } from 'element-plus'
+
+const counter = ref(0)
+
+const TsxCounter = () => {
+  return (
+    <div class="app">
+      <ElButton onClick={() => counter.value--} type='primary'>-1</ElButton>
+      <span>当前计数：{counter.value}</span>
+      <ElButton onClick={() => counter.value++} type='primary'>+1</ElButton>
+    </div>
+  )
+}
 </script>
 
 <template>
   <div class="task-exception">
-    <h2>task-exception</h2>
+    <h2>tsx写的计数器</h2>
+    <TsxCounter />
   </div>
 </template>
 
 <style scoped lang="scss">
-
+.task-exception {
+  padding: 20px;
+}
 </style>
